c965ab5d2d96d024e6305138f7546672861c1a62
Aryan Shrivastava <aryanshrivastava8@gmailcom>
2022-11-13 02:58:58 +0530

    ok

--- projectjs.js
+++ projectjs.js
@@ -1,13 +1,4 @@
-// When the user scrolls the page, execute myFunction
-/*window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-*/
 
 function myFunction() {
   const element = document.getElementById("scrolling");
@@ -15,5 +6,4 @@ function myFunction() {
   document.getElementById ("pixelsscrolled").innerHTML = "Light Years=" + (x.toFixed())*39333/9460000000000+"  Distance=" + (x.toFixed())*39333+"km  " + " Pixels=" + (x.toFixed())+" " ;
 
 
-}
-
+}   
